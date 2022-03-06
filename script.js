03d1dbeeb067e422f5ef6a47a073ca064bba81e9
Adrian Furmanek <adrian.furm@gmail.com>
2022-03-06 10:54:04 +0100

    set and get bookmarks to local storage

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const websiteNameElement = document.getElementById('website-name');
 const websiteUrlElement = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
 
+let bookmarks = [];
+
 function showModal() {
     modal.classList.add('show-modal');
     websiteNameElement.focus();
@@ -14,3 +16,56 @@ function showModal() {
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', (ev) => (ev.target === modal ? modal.classList.remove('show-modal') : false)); 
+
+function validate(nameValue, urlValue) {
+    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    const regex = new RegExp(expression);
+    if (!nameValue || !urlValue) {
+        alert('Please submit values for both fields!');
+        return false;
+    }
+    if (!urlValue.match(regex)) {
+        alert('Please provide a valid web address');
+        return false;
+    }
+    return true;
+}
+
+function fetchBookmarks() {
+    if (localStorage.getItem('bookmarks')) {
+        bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    } else {
+        bookmarks = [
+            {
+                name: 'Github',
+                url: 'https://gitub.com'
+            },
+        ];
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
+    console.log(bookmarks);
+}
+
+function storeBookmark(event) {
+    event.preventDefault();
+    const nameValue = websiteNameElement.value;
+    let urlValue = websiteUrlElement.value;
+    if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
+     urlValue = `https://${urlValue}`; 
+    }
+    if (!validate(nameValue, urlValue)) {
+        return false;
+    }
+    const bookmark = {
+        name: nameValue,
+        url: urlValue
+    };
+    bookmarks.push(bookmark);
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    fetchBookmarks();
+    bookmarkForm.reset();
+    websiteNameElement.focus();
+}
+
+bookmarkForm.addEventListener('submit', storeBookmark);
+fetchBookmarks();
